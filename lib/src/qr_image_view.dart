3f6f75047e81f1eabe68573d782c25263ce4e55b
sadespresso <batmend@gege.mn>
2023-09-02 14:55:08 +0800

    show qr while image is loading

--- lib/src/qr_image_view.dart
+++ lib/src/qr_image_view.dart
@@ -43,7 +43,7 @@ class QrImageView extends StatefulWidget {
     ),
     this.embeddedImageEmitsError = false,
     @Deprecated('use colors in eyeStyle and dataModuleStyle instead')
-        this.foregroundColor,
+    this.foregroundColor,
   })  : assert(
           QrVersions.isSupportedVersion(version),
           'QR code version $version is not supported',
@@ -77,7 +77,7 @@ class QrImageView extends StatefulWidget {
     ),
     this.embeddedImageEmitsError = false,
     @Deprecated('use colors in eyeStyle and dataModuleStyle instead')
-        this.foregroundColor,
+    this.foregroundColor,
   })  : assert(
           QrVersions.isSupportedVersion(version),
           'QR code version $version is not supported',
@@ -184,8 +184,7 @@ class _QrImageViewState extends State<QrImageView> {
           return _errorWidget(context, constraints, _validationResult.error);
         }
         // no error, build the regular widget
-        final widgetSize =
-            widget.size ?? constraints.biggest.shortestSide;
+        final widgetSize = widget.size ?? constraints.biggest.shortestSide;
         if (widget.embeddedImage != null) {
           // if requesting to embed an image then we need to load via a
           // FutureBuilder because the image provider will be async.
@@ -203,7 +202,7 @@ class _QrImageViewState extends State<QrImageView> {
                 final loadedImage = snapshot.data;
                 return _qrWidget(loadedImage, widgetSize);
               } else {
-                return Container();
+                return _qrWidget(null, widgetSize);
               }
             },
           );
